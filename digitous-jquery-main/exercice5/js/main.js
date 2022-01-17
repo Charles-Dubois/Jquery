187c71bbc3fb3516e31b8994256318d7a4775983
Charles <charles.dubois.h@gmail.com>
2022-01-17 14:14:18 +0100

    exo6

--- digitous-jquery-main/exercice5/js/main.js
+++ digitous-jquery-main/exercice5/js/main.js
@@ -1,7 +1,3 @@
-// git add .
-// git commit -m 
-// git pull
-// git push
 $(() => {
     $("#text").addClass("float-right");
 })
---
+++ digitous-jquery-main/exercice6/js/main.js
@@ -0,0 +1,7 @@
+// git add .
+// git commit -m 
+// git pull
+// git push
+$(() => {
+    $("#text").html("<p> <em>Konexio! </em></p>");
+})
